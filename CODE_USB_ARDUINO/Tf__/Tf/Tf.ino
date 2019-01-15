76e063509b8e06c9fd6a8e44dddb2aee6285196b
Gursharan Ahir <42671059+Gursharan-Ahir@users.noreply.github.com>
2019-01-15 10:03:41 -0600

    Add files via upload

---
+++ CODE_USB_ARDUINO/Tf__/Tf/Tf.ino
@@ -0,0 +1,39 @@
+#include "Arduino.h"
+#include <SPI.h>
+#include <RF24.h>
+RF24 radio(7, 8);
+
+const uint64_t pipe= 0xFFFFFFFFFFLL;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("THIS IS THE TRANSMITTER CODE - YOU NEED THE OTHER ARDIUNO TO SEND BACK A RESPONSE");
+  
+  radio.begin();
+
+  
+  radio.setPALevel(RF24_PA_MIN);
+
+  
+  radio.setDataRate(RF24_2MBPS);
+
+  radio.setChannel(1);
+
+  radio.openWritingPipe(pipe);
+ 
+
+  randomSeed(analogRead(A0));
+}
+
+void loop() 
+{
+
+  unsigned char data = random(0, 254);
+    
+  radio.stopListening(); 
+  radio.write(&data,sizeof(data));
+  Serial.print("Sent: ");
+  Serial.println(data);
+  
+  delay(1000);
+}
